b1698ee2ffd541de2c4147636a068dcff276bb5d
priya <sivapragasampriya01@gmail.com>
2025-07-23 11:27:24 +0530

    Add initial structure for InvoiceGenerator app with routing and components

--- InvoiceGenerator/src/App.jsx
+++ InvoiceGenerator/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MenuBar from "./components/MenuBar";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import MainPage from "./pages/MainPage";
+import PreviewPage from "./pages/PreviewPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <MenuBar />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Generate" element={<MainPage />} />
+        <Route path="/preview" element={<PreviewPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
