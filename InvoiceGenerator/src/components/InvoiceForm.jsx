a4de03d5385399cc624cfee3e2e7666e51c89b91
priya <sivapragasampriya01@gmail.com>
2025-07-25 17:34:17 +0530

    Add billing and shipping information fields to InvoiceForm component

--- InvoiceGenerator/src/components/InvoiceForm.jsx
+++ InvoiceGenerator/src/components/InvoiceForm.jsx
@@ -39,12 +39,67 @@ const InvoiceForm = () => {
 
             </div>
             <div className="mb-4">
+                <h5>Bill To</h5>
+                <div className="row g-3">
+                    <div className="col-md-6">
+                        <input type="text" className="form-control" placeholder="Name" />
+                    </div>
+                    <div className="col-md-6">
+                        <input type="text" className="form-control" placeholder="Phone number" />
+                    </div>
+                    <div className="col-md-12">
+                        <input type="text" className="form-control" placeholder="Address" />
+                    </div>
+                </div>
 
             </div>
             <div className="mb-4">
+                <div className="mb-4">
+                    <div className="d-flex justify-content-between align-items-center mb-2">
+                        <h5>Ship To</h5>
+                        <div className="form-check">
+                            <input type="checkbox" className="form-check-input" id="sameAsBilling" />
+                            <label htmlFor="sameAsBilling" className="form-check-label">
+                                Same as Billing
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="row g-3">
+                    <div className="col-md-6">
+                        <input type="text" className="form-control" placeholder="Name" />
+                    </div>
+                    <div className="col-md-6">
+                        <input type="text" className="form-control" placeholder="Phone number" />
+                    </div>
+                    <div className="col-md-12">
+                        <input type="text" className="form-control" placeholder="Shipping Address" />
+                    </div>
+                </div>
+
 
             </div>
             <div className="mb-4">
+                <h5>Invoice Information</h5>
+                <div className="row g-3">
+                    <div className="col-md-4">
+                    <label htmlFor="invoiceNumber" className="form-label">Invoice Number</label>
+
+                        <input type="text" disabled className="form-control" placeholder="Invoice Number" id='invoiceNumber' />
+                    </div>
+                    <div className="col-md-4">
+                    <label htmlFor="invoiceDate" className="form-label">Invoice Date</label>
+
+                        <input type="date" className="form-control" id='invoiceDate' />
+                    </div>
+                    <div className="col-md-4">
+                    <label htmlFor="invoiceDueDate" className="form-label">Invoice Due Date</label>
+
+                        <input type="date" className="form-control"  id='invoiceDueDate'/>
+                    </div>
+                </div>
 
             </div>
             <div className="mb-4">
