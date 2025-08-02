63b5e8ba4c5054e5d43211269ad7a794e11ad80f
priya <sivapragasampriya01@gmail.com>
2025-08-02 13:35:16 +0530

    Add template images and implement TemplateGrid component for template selection

--- InvoiceGenerator/src/assets/assets.js
+++ InvoiceGenerator/src/assets/assets.js
@@ -4,6 +4,13 @@ import landing2 from './landing2.jpeg';
 import landing3 from './landing3.jpeg';
 import landing4 from './landing4.jpeg';
 import uploadarea from './uploadarea.jpeg';
+import Template1 from './Template1.png';
+import Template2 from './Template2.png';
+import Template3 from './Template3.png';
+import Template4 from './Template4.png';
+import Template5 from './Template5.png';
+
+// Export individual assets
 export const assets = {
     logo,
     landing1,
@@ -11,5 +18,18 @@ export const assets = {
     landing3,
     landing4,
     uploadarea,
-    
+    Template1,
+    Template2,
+    Template3,
+    Template4,
+    Template5,
 };
+
+// Define and export the templates array
+export const templates = [
+    { id: 1, label: 'Template 1', image: Template1 },
+    { id: 2, label: 'Template 2', image: Template2 },
+    { id: 3, label: 'Template 3', image: Template3 },
+    { id: 4, label: 'Template 4', image: Template4 },
+    { id: 5, label: 'Template 5', image: Template5 },
+];
