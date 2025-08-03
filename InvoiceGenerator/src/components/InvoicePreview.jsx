767463fb60683c77d64933faea3517e5865d05d2
priya <sivapragasampriya01@gmail.com>
2025-08-03 07:24:47 +0530

    Add Template1 component and associated styles; integrate formatInvoiceData in InvoicePreview

--- InvoiceGenerator/src/components/InvoicePreview.jsx
+++ InvoiceGenerator/src/components/InvoicePreview.jsx
@@ -1,14 +1,15 @@
 import React, { forwardRef } from "react";
-
+import { formatInvoiceData } from "../util/formatInvoice";
+import Template1 from "../templates/Teplate1/Template1";
 const InvoicePreview = forwardRef(({ invoiceData, template }, ref) => {
-    
-  return (
-    <div ref={ref} className="invoice-preview container px-2 py-2 overflow-x-auto">
-      {/* Render the PDF content here using invoiceData and template */}
-      <h2>Invoice Preview</h2>
-      {/* Add more invoice details as needed */}
-    </div>
-  );
+    const formattedData = formatInvoiceData(invoiceData);
+
+    return (
+        <div ref={ref} className="invoice-preview container px-2 py-2 overflow-x-auto">
+            {/* Render the PDF content here using invoiceData and template */}
+            <Template1 data={formattedData} />      {/* Add more invoice details as needed */}
+        </div>
+    );
 });
 
 export default InvoicePreview;
---
+++ InvoiceGenerator/src/templates/Teplate1/Template1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Template1.css' // Import the CSS file for Template1 styles;
+
+const Template1 = ({data}) => {
+  return (
+    <div>Template1</div>
+  )
+}
+
+export default Template1
